64765ea8472fc811bc8eea4e75f035a8a3b2b505
OldMarkie <marc_tasarra@dlsu.edu.ph>
2024-06-06 23:06:42 +0800

    Render victory message

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -1,9 +1,22 @@
-import { mount } from '@vue/test-utils'
-import HelloWorld from '../WordleBoard.vue'
+import { DOMWrapper, VueWrapper, mount } from '@vue/test-utils'
+import WordleBoard from '../WordleBoard.vue'
 
-describe('HelloWorld', () => {
-  it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
+describe('WordleBoard', () => {
+  test("a victory messgae appears when the user makes a guess that matches the word of the day", async(): Promise <void >=> {
+    //arrange
+
+    const wrapper: VueWrapper = mount(WordleBoard, {props: {wordOfTheDay:"TESTS"}})
+
+    //act
+
+    const guessInput: DOMWrapper<Element>  = wrapper.find("input[type=text]")
+    await guessInput.setValue("TESTS")
+    await guessInput.trigger("keydown.enter")
+
+    //assert
+
+    expect(wrapper.text()).toContain("You won!")
+
   })
 })
+ 

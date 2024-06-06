ffd83af9b7c54f9d7748d5eef9cb6bc30ea17667
OldMarkie <marc_tasarra@dlsu.edu.ph>
2024-06-06 23:11:22 +0800

    Refactor

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -1,21 +1,16 @@
 import { DOMWrapper, VueWrapper, mount } from '@vue/test-utils'
 import WordleBoard from '../WordleBoard.vue'
+import { VICTORY_MESSAGE } from '@/settings'
 
 describe('WordleBoard', () => {
   test("a victory messgae appears when the user makes a guess that matches the word of the day", async(): Promise <void >=> {
-    //arrange
-
     const wrapper: VueWrapper = mount(WordleBoard, {props: {wordOfTheDay:"TESTS"}})
-
-    //act
 
     const guessInput: DOMWrapper<Element>  = wrapper.find("input[type=text]")
     await guessInput.setValue("TESTS")
     await guessInput.trigger("keydown.enter")
 
-    //assert
-
-    expect(wrapper.text()).toContain("You won!")
+    expect(wrapper.text()).toContain(VICTORY_MESSAGE)
 
   })
 })

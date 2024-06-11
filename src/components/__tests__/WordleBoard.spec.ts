782aff98e7f61ba750e8224ace85fa02305667fe
OldMarkie <marc_tasarra@dlsu.edu.ph>
2024-06-11 08:49:07 +0800

    Ensure no warnings are rendered when a proper English word is provided

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -57,5 +57,9 @@ describe('WordleBoard', () => {
     expect(console.warn).toHaveBeenCalled()
   })
 
+  test("no wraning is emmitted if the word of the day is a real uppercase English word with 5 chars")
+  console.warn = vi.fn()
+  mount(WordleBoard, {props: {wordOfTheDay: "TESTS"}})
+  expect(console.warn).not.toHaveBeenCalled()
 })
  
